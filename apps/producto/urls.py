5ee47a56d352bbed8f37f1055345a206fd88054e
Camilo_Ts <camil.tapias@gmail.com>
2021-06-14 20:58:42 -0400

    Se adapto de manera visual la pagina de CRUD producto para Django

--- apps/producto/urls.py
+++ apps/producto/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.prueba, name='prueba'),
+    path('prueba/', views.formularioPrueba, name='formulario'),
+    path('crear/', views.agregarProducto, name='crudproducto'),
 ]
