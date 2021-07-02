a7dc800ca5dbe05b66bbe70d4b7d50343423c8c1
Juan Ignacio Diaz Hernandez <ignaciodiazh13@gmail.com>
2021-07-01 21:52:07 -0400

    Reparacion y api

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
-const url = 'https://mindicador.cl/api/dolar'
+(function (d, w) {
+    
+    const url = 'https://mindicador.cl/api/dolar'
     fetch(url)
         .then(response => response.json())
         .then(data => {
@@ -12,3 +14,16 @@ const url = 'https://mindicador.cl/api/dolar'
 
 
     setInterval(function () { mostrarhora() }, 1000);
+
+    function mostrarhora(){
+        let momentoActual = new Date();
+        let hora = momentoActual.getHours();
+        let minuto = momentoActual.getMinutes();
+        let segundo = momentoActual.getSeconds();
+    
+        let horaImprimible = hora + ":" + minuto + ":" + segundo;
+    
+        const contenido_hora = document.getElementById('mostrarhora');
+        contenido_hora.innerText = horaImprimible ;
+    }
+}) (document, window);
