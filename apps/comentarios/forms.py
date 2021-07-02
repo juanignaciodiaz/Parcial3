5178ef8cfc1bffc6f2bf6dd534d7e178bbd8bbd1
ReneRetamales <re.retamales@duocuc.cl>
2021-07-02 03:43:54 -0400

    Se habilitó la autenticación en la sección de comentarios

--- apps/comentarios/forms.py
+++ apps/comentarios/forms.py
@@ -5,6 +5,6 @@ class ComentarioBarra(forms.ModelForm):
     class Meta:
         model = Comentario
         widgets = {
-            'descripcion': forms.Textarea(attrs={"class":"form-control","placeholder":"Agregar comentario...","maxlength":"399","minlength":"19"})
+            'descripcion': forms.Textarea(attrs={"class":"form-control","placeholder":"Agregar comentario...","maxlength":"499","minlength":"19"})
         }
         fields = ('descripcion',)
--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -4,5 +4,5 @@ from apps.cuentas.models import Perfil
 # Create your models here.
 class Comentario(models.Model):
     perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
-    descripcion = models.TextField('Caja de Comentarios', max_length=199, blank=False, null=False)
+    descripcion = models.TextField('Caja de Comentarios', max_length=499, blank=False, null=False)
     fecha = models.DateTimeField('Fecha de Subida', auto_now_add=True)
