f3623bf85cfc9c8281df1adad5fc196c10b503e5
ReneRetamales <re.retamales@duocuc.cl>
2021-07-02 01:22:18 -0400

    Agregado el formulario... falta hacer que se visualice

--- apps/comentarios/models.py
+++ apps/comentarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from apps.cuentas.models import Perfil
 
 # Create your models here.
+class Comentario(models.Model):
+    perfil = models.ForeignKey(Perfil, on_delete=models.CASCADE)
+    descripcion = models.TextField('Descripción', max_length=199, blank=False, null=False)
+    fecha = models.DateTimeField(auto_now_add=True)
--- apps/comentarios/views.py
+++ apps/comentarios/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import redirect, render
+from .forms import ComentarioBarra
 
 def comentario(request):
     return render(request, 'pages/comentarios.html')
